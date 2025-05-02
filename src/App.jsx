b00fbf073ce356c4461e2625e85dced0924c40ce
Alexey <Al.chef.Titov@yandex.ru>
2025-05-03 00:13:42 +0300

    Начало

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Div = styled.div`
+text-align: center;
+`;
+
+export const App = () => (
+	<Div>
+		<i className="fa fa-briefcase"></i>
+		<div>456</div>
+	</Div>
+);
