e9fba2051120ce4af20a39904947813f9545a50d
Alexey <Al.chef.Titov@yandex.ru>
2025-05-20 16:04:05 +0300

    Реализовать страницу авторизации

---
+++ src/components/button/button.jsx
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+const ButtonContainer = ({ children, className, ...props }) => {
+	return (
+		<button className={className} {...props}>
+			{children}
+		</button>
+	);
+};
+
+export const Button = styled(ButtonContainer)`
+	display: flex;
+	-webkit-box-pack: center;
+	justify-content: center;
+	-webkit-box-align: center;
+	aling-items: center;
+	font-size: 18px;
+	width: ${({ width = '100%' }) => width};
+	height: 32px;
+	border: 1px solid rgb(0 0 0);
+	background-color: rgb(238, 238, 238);
+
+	&:hover {
+		cursor: pointer;
+	}
+`;
+
+// text-align: center;
---
+++ src/components/h2/h2.jsx
@@ -0,0 +1,9 @@
+import slyled from 'styled-components';
+
+const H2Container = ({ children, className }) => {
+	<h2 className={className}>{children}</h2>;
+};
+
+export const H2 = slyled(H2Container)`
+margin: 40px 0;
+`;
