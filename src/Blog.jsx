1969120bcbf4ef4524a5297ea20136341090ccff
Alexey <Al.chef.Titov@yandex.ru>
2025-06-03 14:22:28 +0300

    Реализовать поиск на Главной

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -10,6 +10,7 @@ const AppColumn = styled.div`
 	display: flex;
 	flex-direction: column;
 	justify-content: space-between;
+    position; relative;
 	width: 1000px;
 	min-height: 100%;
 	margin: 0 auto;
@@ -22,6 +23,7 @@ const Page = styled.div`
 
 export const Blog = () => {
 	const dispatch = useDispatch();
+
 	useLayoutEffect(() => {
 		const currentUserDataJSON = sessionStorage.getItem('userData');
 
